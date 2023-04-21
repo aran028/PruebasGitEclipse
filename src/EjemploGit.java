53dc845689de703c114486a55c08c5e16ad0249c
Aranzazu Foronda <afa028@gmail.com>
2023-04-21 21:56:47 +0200

    Se imprime el nombre de usuario

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Git desde eclipse");
 		String nombre="Arancha";
+		System.out.println(nombre);
 
 	}
 
